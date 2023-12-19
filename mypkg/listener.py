adf925bc7c2fa7038dccf4902a43c6111768749d
KM <blackgodstone.k@gmail.com>
2023-12-19 21:52:22 +0900

    create timer

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,10 +1,14 @@
 import rclpy
+import math
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
 def cb(msg):
     global node
-    node.get_logger().info("Listen: %d" % msg.data)
+    hour = math.floor(msg.data / 3600)
+    minute = math.floor(( msg.data / 60 ) % 60)
+    second = math.floor(msg.data % 60)
+    node.get_logger().info("Listen: %d h %d m %d s" % (hour,minute,second))
 
 rclpy.init()
 node = Node("listener")
